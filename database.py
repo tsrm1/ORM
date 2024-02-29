f752d8f7b1ab3e5b231d7aec22700dea6a8a4e16
R()M)(N <111087019+tsrm1@users.noreply.github.com>
2024-02-29 20:57:03 +0100

    first commit

---
+++ database.py
@@ -0,0 +1,30 @@
+# pip install sqlalchemy
+# pip install psycopg2
+# pip install python-dotenv
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+## For sqlite
+
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+
+## For postgresql
+# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+SQLALCHEMY_DATABASE_URL = os.getenv("POSTGRESQL_URL")
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+conn = engine.connect()
+print(conn.get_isolation_level())
