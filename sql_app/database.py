8b81fb9aa301690ca4c5685e2692db6c560aca34
R()M)(N <111087019+tsrm1@users.noreply.github.com>
2024-03-01 21:43:45 +0100

    API server. CRUD function

--- sql_app/database.py
+++ sql_app/database.py
@@ -6,10 +6,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-import os
-from dotenv import load_dotenv
+# import os
+# from dotenv import load_dotenv
+# load_dotenv()
 
-load_dotenv()
+from sql_app.db_url import SQLALCHEMY_DATABASE_URL
 
 ## For sqlite
 
@@ -18,8 +19,10 @@ load_dotenv()
 
 ## For postgresql
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
-SQLALCHEMY_DATABASE_URL = os.getenv("POSTGRESQL_URL")
+# SQLALCHEMY_DATABASE_URL = os.getenv("POSTGRESQL_URL")
+# SQLALCHEMY_DATABASE_URL = os.getenv("POSTGRESQL_URL")
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
---
+++ sql_app/db_url.py
@@ -0,0 +1,1 @@
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Katran4ik@localhost/Fast_API_study"
